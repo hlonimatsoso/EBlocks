b2b03a280fb577bd1b29226e307b5188614cd020
hloni2010 <hloni2010@gmail.com>
2020-08-24 16:36:37 +0200

    Revamped tables

--- EBlocks.Api/Repos/DemoSupplierRepo.cs
+++ EBlocks.Api/Repos/DemoSupplierRepo.cs
@@ -16,9 +16,9 @@ namespace EBlocks.Api.Repos
         public DemoSupplierRepo()
         {
             this._Suppliers = new List<ISupplier> {
-                new Supplier{CompanyName = "B M W",City="Bavaria",ContactName="M Division Boss",ContactTitle="Prof", Region="Gauteng", SupplierID = new Guid("f5205e63-069e-4c97-a1a3-aad77c92a769")},
-                new Supplier{CompanyName = "Benz",City="Frankfurt",ContactName="AMG Boss",ContactTitle="Mr", Region="Gauteng" , SupplierID = new Guid("b885f01f-ea23-4c9a-948d-a9f667bebd2b")},
-                new Supplier{CompanyName = "Porsche",City="Studguard",ContactName="Turbo S Boss",ContactTitle="Dr", Region="Cape Town" , SupplierID = new Guid("53903e1c-b59e-4d6f-9dc1-0c761ed92d2b")}
+                new Supplier{CompanyName = "B M W",City="Bavaria",ContactName="M Division Boss",ContactTitle="Prof", Region="Gauteng", SupplierID = new Guid("f5205e63-069e-4c97-a1a3-aad77c92a769"),HomePage="www.bmw.co.za"},
+                new Supplier{CompanyName = "Benz",City="Frankfurt",ContactName="AMG Boss",ContactTitle="Mr", Region="Gauteng" , SupplierID = new Guid("b885f01f-ea23-4c9a-948d-a9f667bebd2b"),HomePage="www.benz.co.za"},
+                new Supplier{CompanyName = "Porsche",City="Studguard",ContactName="Turbo S Boss",ContactTitle="Dr", Region="Cape Town" , SupplierID = new Guid("53903e1c-b59e-4d6f-9dc1-0c761ed92d2b"),HomePage="www.ofporsche.co.za"}
 
 
             };
